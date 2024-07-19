11b6ecc44f559e82b17ddb8a78a5db2210c703e3
Nikolai Merritt <nikolai.merritt@withsecure.com>
2024-07-19 17:45:37 +0100

    Wip on typescript + vue stufff

---
+++ wwwroot/ts/webpack.dev.ts
@@ -0,0 +1,50 @@
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+
+module.exports = {
+    entry: {
+        labs: {
+            import: "./src/labs/main.ts",
+            dependOn: "endpoints",
+        },
+        endpoints: {
+            import: "./src/endpoints/main.ts",
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s?css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+                options: {
+                    appendTsSuffixTo: [/\.vue$/],
+                },
+            },
+            {
+                test: /\.vue$/,
+                use: "vue-loader",
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js", ".vue"],
+        alias: {
+            vue$: "vue/dist/vue.esm.js",
+        },
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    optimization: {
+        runtimeChunk: "single",
+    },
+    plugins: [new VueLoaderPlugin()],
+        mode: "development",
+    devtool: "inline-source-map",
+};
